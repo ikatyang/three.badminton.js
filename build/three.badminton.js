c84636084c4edd6c125f7689dba435d23604d0ea
Ika <ikatyang@gmail.com>
2016-11-02 20:29:00 +0800

    Update builds

--- build/three.badminton.js
+++ build/three.badminton.js
@@ -26,63 +26,180 @@ if (THREE.Object3D.prototype.localToTarget === undefined) {
 	}
 }
 
-function Shuttle(corkRadius, skirtRadius, skirtHeight, corkMass, skirtMass) {
+function ShuttlecockGeometry(corkRadius, skirtRadius, beltHeight, skirtHeight, widthSegments, heightSegments, rachisRadius, rachisRatio, featherWidth, featherRatio, featherAngle, massRatio) {
 	
-	THREE.Object3D.call(this);
+	THREE.Geometry.call(this);
 	
-	corkMass = (corkMass !== undefined) ? corkMass : 0.005;
-	skirtMass = (skirtMass !== undefined) ? skirtMass : 0.0001;
+	this.type = 'ShuttlecockGeometry';
+	
+	var R = skirtRadius;
+	var r = corkRadius;
+	var h = skirtHeight;
+	var b = beltHeight;
+	
+	var corkMassCenter = (5 * r * r + 12 * b * r + 6 * b * b) / (4 * (2 * r + 3 * b));
+	var skirtMassCenter = (h / 4) * (r * r + 2 * R * r + 3 * R * R) / (r * r + R * r + R * R);
+	
+	var massCenter = (massRatio === undefined) ? corkRadius + beltHeight :
+		(corkMassCenter * massRatio + skirtMassCenter) / (massRatio + 1);
+	
+	var corkAngle = Math.atan((R - r) / (b + h));
+	var massToCorkTopLength = r / Math.tan(corkAngle);
+	var massToCorkCenterLength = massCenter - corkMassCenter;
+	
+	var skirtCrossSectionalRadius = r + (R - r) * (skirtMassCenter / h);
+	var skirtCrossSectionalArea = Math.PI * skirtCrossSectionalRadius * skirtCrossSectionalRadius;
 	
 	this.parameters = {
 		corkRadius: corkRadius,
-		skirtRadius: skirtRadius, 
+		skirtRadius: skirtRadius,
+		beltHeight: beltHeight,
 		skirtHeight: skirtHeight,
+		widthSegments: widthSegments,
+		heightSegments: heightSegments,
+		rachisRadius: rachisRadius,
+		rachisRatio: rachisRatio,
+		featherWidth: featherWidth,
+		featherRatio: featherRatio,
+		featherAngle: featherAngle,
+		massRatio: massRatio,
+		massCenter: massCenter,
+		corkMassCenter: corkMassCenter,
+		skirtMassCenter: skirtMassCenter,
+		corkAngle: corkAngle,
+		massToCorkTopLength: massToCorkTopLength,
+		massToCorkCenterLength: massToCorkCenterLength,
+		skirtCrossSectionalArea: skirtCrossSectionalArea
+	};
+	
+	var corkPositionY = -corkRadius + massCenter;
+	var corkGeometry = new THREE.SphereGeometry(corkRadius, widthSegments, heightSegments, 0, Math.PI * 2, 0, Math.PI / 2);
+	this.merge(corkGeometry, new THREE.Matrix4().compose(
+		new THREE.Vector3(0, corkPositionY, 0),
+		new THREE.Quaternion().setFromEuler(new THREE.Euler(0, Math.PI / 2, 0, 'XYZ')),
+		new THREE.Vector3(1, 1, 1)
+	), 0);
+	
+	var beltPositionY = corkPositionY - beltHeight / 2;
+	var beltGeometry = new THREE.CylinderGeometry(corkRadius, corkRadius, beltHeight, widthSegments, 1, true, 0, Math.PI * 2);
+	this.merge(beltGeometry, new THREE.Matrix4().compose(
+		new THREE.Vector3(0, beltPositionY, 0),
+		new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0, 'XYZ')),
+		new THREE.Vector3(1, 1, 1)
+	), 1);
+	
+	var circlePositionY = beltPositionY - beltHeight / 2;
+	var circleGeometry = new THREE.CircleGeometry(corkRadius, widthSegments, 0, Math.PI * 2);
+	this.merge(circleGeometry, new THREE.Matrix4().compose(
+		new THREE.Vector3(0, circlePositionY, 0),
+		new THREE.Quaternion().setFromEuler(new THREE.Euler(Math.PI / 2, 0, 0, 'XYZ')),
+		new THREE.Vector3(1, 1, 1)
+	), 2);
+	
+	var corkInnerRadius = corkRadius - rachisRadius;
+	
+	var ringPositionY1 = circlePositionY - (skirtHeight * rachisRatio / (1 + rachisRatio));
+	var ringRadius1 = corkInnerRadius + (skirtRadius - corkInnerRadius) * rachisRatio / (1 + rachisRatio);
+	var ringGeometry1 = new THREE.TorusGeometry(ringRadius1, rachisRadius, heightSegments * 2, widthSegments, Math.PI * 2);
+	this.merge(ringGeometry1, new THREE.Matrix4().compose(
+		new THREE.Vector3(0, ringPositionY1, 0),
+		new THREE.Quaternion().setFromEuler(new THREE.Euler(Math.PI / 2, 0, 0, 'XYZ')),
+		new THREE.Vector3(1, 1, 1)
+	), 3);
+	
+	var ringPositionY2 = (circlePositionY + ringPositionY1) / 2;
+	var ringRadius2 = (corkInnerRadius + ringRadius1) / 2;
+	var ringGeometry2 = new THREE.TorusGeometry(ringRadius2, rachisRadius, heightSegments * 2, widthSegments, Math.PI * 2);
+	this.merge(ringGeometry2, new THREE.Matrix4().compose(
+		new THREE.Vector3(0, ringPositionY2, 0),
+		new THREE.Quaternion().setFromEuler(new THREE.Euler(Math.PI / 2, 0, 0, 'XYZ')),
+		new THREE.Vector3(1, 1, 1)
+	), 3);
+	
+	var rachisPositionRadius = (corkInnerRadius + skirtRadius) / 2;
+	var rachisAngle = -Math.atan((skirtRadius - corkInnerRadius) / skirtHeight);
+	var rachisHeight = skirtHeight / Math.cos(rachisAngle);
+	var rachisPositionY = circlePositionY - (skirtHeight / 2);
+	var rachisGeometry = new THREE.CylinderGeometry(rachisRadius, rachisRadius, rachisHeight, widthSegments, 1, false, 0, Math.PI * 2);
+	for (var i = 0; i < rachisGeometry.faces.length; i++)
+		rachisGeometry.faces[i].materialIndex = 0;
+	
+	var featherHeight = rachisHeight * (1 / (1 + rachisRatio));
+	var featherGeometry = new THREE.Geometry();
+	featherGeometry.vertices.push(
+		new THREE.Vector3(0, 0, 0),
+		new THREE.Vector3(featherWidth, -featherHeight * (featherRatio / (1 + featherRatio)), 0),
+		new THREE.Vector3(-featherWidth, -featherHeight * (featherRatio / (1 + featherRatio)), 0),
+		new THREE.Vector3(0, -featherHeight, 0)
+	);
+	featherGeometry.faces.push(
+		new THREE.Face3(0, 2, 3),
+		new THREE.Face3(0, 3, 1)
+	);
+	featherGeometry.faceVertexUvs[0].push(
+		[new THREE.Vector2(0, 1), new THREE.Vector2(0, 0), new THREE.Vector2(1, 0)],
+		[new THREE.Vector2(1, 1), new THREE.Vector2(0, 0), new THREE.Vector2(1, 0)]
+	);
+	featherGeometry.computeFaceNormals();
+	featherGeometry.computeVertexNormals();
+	
+	for (var i = 0; i < widthSegments; i++) {
+		var theta = Math.PI * 2 / widthSegments * i;
+		var rachisPositionX = Math.sin(theta) * rachisPositionRadius;
+		var rachisPositionZ = Math.cos(theta) * rachisPositionRadius;
+		var rachisEuler = new THREE.Euler(rachisAngle, theta, 0, 'YXZ');
+		this.merge(rachisGeometry, new THREE.Matrix4().compose(
+			new THREE.Vector3(rachisPositionX, rachisPositionY, rachisPositionZ),
+			new THREE.Quaternion().setFromEuler(rachisEuler),
+			new THREE.Vector3(1, 1, 1)
+		), 3);
+		var featherPositionX = Math.sin(theta) * ringRadius1;
+		var featherPositionZ = Math.cos(theta) * ringRadius1;
+		var featherMatrix = new THREE.Matrix4().makeRotationFromEuler(rachisEuler)
+			.multiply(new THREE.Matrix4().makeRotationY(featherAngle));
+		this.merge(featherGeometry, new THREE.Matrix4().compose(
+			new THREE.Vector3(featherPositionX, ringPositionY1, featherPositionZ),
+			new THREE.Quaternion().setFromRotationMatrix(featherMatrix),
+			new THREE.Vector3(1, 1, 1)
+		), 4);
+	}
+}
+
+ShuttlecockGeometry.prototype = Object.create(THREE.Geometry.prototype);
+ShuttlecockGeometry.prototype.constructor = ShuttlecockGeometry;
+
+function Shuttlecock(geometry, material, corkMass, skirtMass, corkAngle, massToCorkTopLength, massToCorkCenterLength, skirtCrossSectionalArea) {
+	
+	THREE.Object3D.call(this);
+	
+	if (geometry instanceof ShuttlecockGeometry) {
+		corkAngle = geometry.parameters.corkAngle;
+		massToCorkTopLength = geometry.parameters.massToCorkTopLength;
+		massToCorkCenterLength = geometry.parameters.massToCorkCenterLength;
+		skirtCrossSectionalArea = geometry.parameters.skirtCrossSectionalArea;
+	}
+	
+	this.geometry = geometry;
+	this.material = material;
+	
+	this.parameters = {
+		mass: corkMass + skirtMass,
 		corkMass: corkMass,
 		skirtMass: skirtMass,
+		corkAngle: corkAngle,
+		massToCorkTopLength: massToCorkTopLength,
+		massToCorkCenterLength: massToCorkCenterLength,
+		skirtCrossSectionalArea: skirtCrossSectionalArea
 	};
 	
 	var flipFrame = new THREE.Object3D();
 	this.add(flipFrame);
 	
-	var cork = new THREE.Mesh(
-		new THREE.SphereGeometry(corkRadius, 8, 8, 0, Math.PI * 2, 0, Math.PI / 2),
-		new THREE.MeshNormalMaterial({
-			wireframe: true,
-			side: THREE.DoubleSide,
-		}));
-	flipFrame.add(cork);
-	
-	var skirt = new THREE.Mesh(
-		new THREE.CylinderGeometry(corkRadius, skirtRadius, skirtHeight, 16, 1, true),
-		new THREE.MeshNormalMaterial({
-			wireframe: true,
-			side: THREE.DoubleSide,
-		}));
-	flipFrame.add(skirt);
+	var mesh = new THREE.Mesh(geometry, material);
+	flipFrame.add(mesh);
 	
 	this.flipFrame = flipFrame;
-	this.cork = cork;
-	this.skirt = skirt;
-	
-	var corkMassCenter = (5 / 8) * corkRadius;
-	var skirtMassCenter = corkRadius + skirtHeight * (1 - (skirtRadius + corkRadius * 2) / (skirtRadius + corkRadius) / 3);
-	
-	var mass = corkMass + skirtMass;
-	this.mass = mass;
-	
-	var massCenter = (corkMass / mass) * corkMassCenter + (skirtMass / mass) * skirtMassCenter;
-	cork.position.y = massCenter - corkRadius;
-	skirt.position.y = massCenter - corkRadius - skirtHeight / 2;
-	
-	var corkHeight = corkRadius * skirtHeight / (skirtRadius - corkRadius);
-	var fullHeight = corkHeight + skirtHeight;
-	this.corkAngle = Math.atan(skirtRadius / fullHeight);
-	
-	var skirtCrossSectionalRadius = corkRadius + (skirtMassCenter - corkRadius) / skirtHeight * (skirtRadius - corkRadius);
-	this.skirtCrossSectionalArea = Math.PI * skirtCrossSectionalRadius * skirtCrossSectionalRadius;
-	
-	this.corkConeY = massCenter - corkRadius + corkHeight;
-	this.gravityToCorkLength = massCenter - corkMassCenter;
+	this.mesh = mesh;
 	
 	this.dragCoefficient = 0.44;
 	this.groundAttenuation = 0.9;
@@ -103,9 +220,9 @@ function Shuttle(corkRadius, skirtRadius, skirtHeight, corkMass, skirtMass) {
 	this.impactCount = 0;
 }
 
-Shuttle.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Object3D.prototype), {
+Shuttlecock.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Object3D.prototype), {
 
-	constructor: Shuttle,
+	constructor: Shuttlecock,
 	
 	getYAxis: function () {
 		this.flipFrame.updateMatrixWorld();
@@ -150,16 +267,16 @@ Shuttle.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Ob
 	updateMove: function (delta) {
 		
 		var rho = this.airDensity;
-		var S = this.skirtCrossSectionalArea;
+		var S = this.parameters.skirtCrossSectionalArea;
 		var C_D = this.dragCoefficient;
 		var U = this.velocity.length();
 		
 		var F_D = rho * S * C_D * U * U / 2;
 		
 		var Fv = this.velocity.clone().normalize().multiplyScalar(-F_D);
-		var force = this.gravity.clone().multiplyScalar(this.mass).add(Fv);
+		var force = this.gravity.clone().multiplyScalar(this.parameters.mass).add(Fv);
 		
-		this.velocity.addScaledVector(force, delta / this.mass);
+		this.velocity.addScaledVector(force, delta / this.parameters.mass);
 		this.position.addScaledVector(this.velocity, delta);
 		
 		var xAxis = this.velocity.clone().cross(this.gravity).normalize();
@@ -202,12 +319,12 @@ Shuttle.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Ob
 			
 		var flipAxis = this.directionWorldToLocal(yAxis.clone().negate().cross(velocityXZ)).normalize();
 		var flipAngle = Math.min(this.stopAngularVelocity * delta, 
-			yAxis.clone().negate().angleTo(velocityXZ) - this.corkAngle);
+			yAxis.clone().negate().angleTo(velocityXZ) - this.parameters.corkAngle);
 		
 		var flipMatrix = new THREE.Matrix4().makeRotationFromEuler(this.rotation);
 		this.rotation.setFromRotationMatrix(flipMatrix.multiply(new THREE.Matrix4().makeRotationAxis(flipAxis, flipAngle)));
 		
-		this.position.y -= this.localToTarget(new THREE.Vector3(0, this.corkConeY, 0), this.parent).y;
+		this.position.y -= this.localToTarget(new THREE.Vector3(0, this.parameters.massToCorkTopLength, 0), this.parent).y;
 		
 		if (flipAngle < 1e-4)
 			this.state = 'stop-ground';
@@ -221,8 +338,8 @@ Shuttle.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Ob
 		var U = this.velocity.length();
 		var M_B = this.parameters.skirtMass;
 		var M_C = this.parameters.corkMass;
-		var l_GC = this.gravityToCorkLength;
-		var S = this.skirtCrossSectionalArea;
+		var l_GC = this.parameters.massToCorkCenterLength;
+		var S = this.parameters.skirtCrossSectionalArea;
 		return [
 			/* phi_dot     */ phi_dot,
 			/* phi_dot_dot */ -(rho * S * C_D * U) / (2 * M_B * (1 + M_B / M_C)) * phi_dot - (rho * S * C_D * U * U) / (2 * (M_C + M_B) * l_GC) * Math.sin(phi)
@@ -770,7 +887,7 @@ Robot.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Obje
 		link.angleB = 0;
 		
 		link.updateMatrixWorld();
-		if (this.checkIntersect(racket, this.shuttle.cork) && 
+		if (this.checkIntersect(racket, this.shuttle) && 
 			this.impactClock.getDelta() > this.impactDelta) {
 			var normal = link.directionLocalToWorld(new THREE.Vector3(0, 0, 1)).normalize();
 			this.shuttle.impact(normal.clone().multiplyScalar(impactSpeed * impactLength), normal, this.racketAttenuation);
@@ -797,16 +914,22 @@ Robot.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Obje
 		this.body.rotation.y += bodyAngleDelta;
 	},
 	
-	checkIntersect: function (box, sphere) {
+	checkIntersect: function (racket, shuttlecock) {
 			
-		box.updateMatrixWorld();
-		sphere.updateMatrixWorld();
+		racket.updateMatrixWorld();
+		shuttlecock.updateMatrixWorld();
 		
-		var spherePosition = sphere.localToTarget(new THREE.Vector3(0, 0, 0), this.parent);
+		if (shuttlecock.geometry.boundingSphere === null)
+			shuttlecock.geometry.computeBoundingSphere();
+		
+		if (racket.geometry.boundingBox === null)
+			racket.geometry.computeBoundingBox();
+		
+		var spherePosition = shuttlecock.localToTarget(shuttlecock.geometry.boundingSphere.center.clone(), this.parent);
 		var lastSpherePosition = spherePosition.clone().addScaledVector(this.shuttle.velocity, -this.shuttle.lastDelta);
 		
-		this.parent.localToTarget(spherePosition, box);
-		this.parent.localToTarget(lastSpherePosition, box);
+		this.parent.localToTarget(spherePosition, racket);
+		this.parent.localToTarget(lastSpherePosition, racket);
 		
 		var isIntersect = false;
 		
@@ -816,14 +939,14 @@ Robot.prototype = Object.defineProperties(Object.assign(Object.create(THREE.Obje
 			var circlePosition = spherePosition.clone().addScaledVector(spherePositionDelta, mul);
 			isIntersect = this.checkIntersectPlaneAndCircle({
 				min: {
-					x: -box.geometry.parameters.width / 2 - circlePosition.x,
-					y: -box.geometry.parameters.height / 2 - circlePosition.y
+					x: racket.geometry.boundingBox.min.x - circlePosition.x,
+					y: racket.geometry.boundingBox.min.y - circlePosition.y
 				},
 				max: {
-					x: box.geometry.parameters.width / 2 - circlePosition.x,
-					y: box.geometry.parameters.height / 2 - circlePosition.y
+					x: racket.geometry.boundingBox.max.x - circlePosition.x,
+					y: racket.geometry.boundingBox.max.y - circlePosition.y
 				},
-			}, sphere.geometry.parameters.radius);
+			}, shuttlecock.geometry.boundingSphere.radius);
 			this.lastCheckMul = mul;
 		}
 		return isIntersect;
@@ -1293,81 +1416,6 @@ Court.prototype = Object.assign(Object.create(THREE.LineSegments.prototype), {
 	
 });
 
-function Game(court, shuttle, firstPlayer) {
-
-	this.court = court;
-	this.shuttle = shuttle;
-	
-	this.nthScore = 0;
-	this.score1 = 0;
-	this.score2 = 0;
-	this.lastWinner = (firstPlayer !== undefined) ? firstPlayer : 1;
-}
-
-Game.prototype = {
-
-	constructor: Game,
-	
-	get lastWinnerScore() {
-		return this['score' + this.lastWinner];
-	},
-	set lastWinnerScore(value) {
-		this['score' + this.lastWinner] = value;
-	},
-	
-	nextScore: function () {
-		this.nthScore++;
-		this.shuttle.impactCount = 0;
-		this.shuttle.state = 'move';
-	},
-	
-	update: function (delta) {
-		if (this.nthScore !== this.score1 + this.score2) {
-			if (this.shuttle.state === 'stop-net') {
-				if (this.shuttle.impactCount % 2 === 0) {
-					this.lastWinner = this.lastWinner;
-				} else {
-					this.lastWinner = this.lastWinner % 2 + 1;
-				}
-				this.lastWinnerScore++;
-				this.onScoreChange();
-			} else if (this.shuttle.state === 'stop-ground') {
-				var area = (this.shuttle.impactCount <= 1) ?
-					((this.lastWinner === 1) ?
-						this.court.getFirstArea2(this.score1) : 
-						this.court.getFirstArea1(this.score2)) :
-					((this.lastWinner === 1) ?
-						((this.shuttle.impactCount % 2 === 1) ?
-							this.court.getArea2() :
-							this.court.getArea1()) : 
-						((this.shuttle.impactCount % 2 === 1) ?
-							this.court.getArea1() :
-							this.court.getArea2()));
-				var position = this.shuttle.cork.localToTarget(new THREE.Vector3(0, 0, 0), this.court);
-				if (position.x >= area.xMin && position.x <= area.xMax &&
-					position.z >= area.zMin && position.z <= area.zMax) {
-					if (this.shuttle.impactCount % 2 === 1) {
-						this.lastWinner = this.lastWinner;
-					} else {
-						this.lastWinner = this.lastWinner % 2 + 1;
-					}
-				} else {
-					if (this.shuttle.impactCount % 2 === 0) {
-						this.lastWinner = this.lastWinner;
-					} else {
-						this.lastWinner = this.lastWinner % 2 + 1;
-					}
-				}
-				this.lastWinnerScore++;
-				this.onScoreChange();
-			}
-		}
-	},
-	
-	onScoreChange: function () {},
-	
-};
-
 function ScoreBoardCard(width, height, font) {
 
 	THREE.Object3D.call(this);
@@ -1652,13 +1700,89 @@ TargetPoint.prototype = Object.assign(Object.create(THREE.Object3D.prototype), {
 	
 });
 
-exports.Shuttle = Shuttle;
+function Game(court, shuttle, firstPlayer) {
+
+	this.court = court;
+	this.shuttle = shuttle;
+	
+	this.nthScore = 0;
+	this.score1 = 0;
+	this.score2 = 0;
+	this.lastWinner = (firstPlayer !== undefined) ? firstPlayer : 1;
+}
+
+Game.prototype = {
+
+	constructor: Game,
+	
+	get lastWinnerScore() {
+		return this['score' + this.lastWinner];
+	},
+	set lastWinnerScore(value) {
+		this['score' + this.lastWinner] = value;
+	},
+	
+	nextScore: function () {
+		this.nthScore++;
+		this.shuttle.impactCount = 0;
+		this.shuttle.state = 'move';
+	},
+	
+	update: function (delta) {
+		if (this.nthScore !== this.score1 + this.score2) {
+			if (this.shuttle.state === 'stop-net') {
+				if (this.shuttle.impactCount % 2 === 0) {
+					this.lastWinner = this.lastWinner;
+				} else {
+					this.lastWinner = this.lastWinner % 2 + 1;
+				}
+				this.lastWinnerScore++;
+				this.onScoreChange();
+			} else if (this.shuttle.state === 'stop-ground') {
+				var area = (this.shuttle.impactCount <= 1) ?
+					((this.lastWinner === 1) ?
+						this.court.getFirstArea2(this.score1) : 
+						this.court.getFirstArea1(this.score2)) :
+					((this.lastWinner === 1) ?
+						((this.shuttle.impactCount % 2 === 1) ?
+							this.court.getArea2() :
+							this.court.getArea1()) : 
+						((this.shuttle.impactCount % 2 === 1) ?
+							this.court.getArea1() :
+							this.court.getArea2()));
+				var position = this.shuttle.localToTarget(new THREE.Vector3(0, 0, 0), this.court);
+				if (position.x >= area.xMin && position.x <= area.xMax &&
+					position.z >= area.zMin && position.z <= area.zMax) {
+					if (this.shuttle.impactCount % 2 === 1) {
+						this.lastWinner = this.lastWinner;
+					} else {
+						this.lastWinner = this.lastWinner % 2 + 1;
+					}
+				} else {
+					if (this.shuttle.impactCount % 2 === 0) {
+						this.lastWinner = this.lastWinner;
+					} else {
+						this.lastWinner = this.lastWinner % 2 + 1;
+					}
+				}
+				this.lastWinnerScore++;
+				this.onScoreChange();
+			}
+		}
+	},
+	
+	onScoreChange: function () {},
+	
+};
+
+exports.ShuttlecockGeometry = ShuttlecockGeometry;
+exports.Shuttlecock = Shuttlecock;
 exports.Robot = Robot;
 exports.Court = Court;
-exports.Game = Game;
 exports.ScoreBoard = ScoreBoard;
 exports.ScoreBoardCard = ScoreBoardCard;
 exports.TargetPoint = TargetPoint;
+exports.Game = Game;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
