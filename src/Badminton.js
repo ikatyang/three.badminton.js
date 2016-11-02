f778c3925aa77cff3d73859c2f7d16c0da60e67d
Ika <ikatyang@gmail.com>
2016-11-02 23:36:39 +0800

    Modify ScoreBoard to Scoreboard

--- src/Badminton.js
+++ src/Badminton.js
@@ -5,8 +5,8 @@ export { ShuttlecockGeometry } from './geometries/ShuttlecockGeometry.js';
 export { Shuttlecock } from './objects/Shuttlecock.js';
 export { Robot } from './objects/Robot.js';
 export { Court } from './objects/Court.js';
-export { ScoreBoard } from './objects/Scoreboard.js';
-export { ScoreBoardCard } from './objects/ScoreboardCard.js';
+export { Scoreboard } from './objects/Scoreboard.js';
+export { ScoreboardCard } from './objects/ScoreboardCard.js';
 export { TargetPoint } from './objects/TargetPoint.js';
 
 export { Game } from './utils/Game.js';
