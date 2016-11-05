0d987b055106a3e73aece06f9927c677e983c5ee
ikatyang <ikatyang@gmail.com>
2016-11-06 00:10:37 +0800

    Add Box3 plugins

--- src/plugins.js
+++ src/plugins.js
@@ -15,7 +15,26 @@ if (THREE.Object3D.prototype.directionWorldToLocal === undefined) {
 }
 
 if (THREE.Object3D.prototype.localToTarget === undefined) {
-	THREE.Object3D.prototype.localToTarget = function (vector, target) {
-		return target.worldToLocal(this.localToWorld(vector));
+	THREE.Object3D.prototype.localToTarget = function (object, target) {
+		if (object instanceof THREE.Vector3) {
+			return target.worldToLocal(this.localToWorld(object));
+		} else if (object instanceof THREE.Box3) {
+			var p1 = this.localToTarget(object.min.clone(), target);
+			var p2 = this.localToTarget(object.max.clone(), target);
+			return object.setFromPoints([p1, p2]);
+		}
+	}
+}
+
+if (THREE.Box3.prototype.fromArray === undefined) {
+	THREE.Box3.prototype.fromArray = function (array) {
+		this.setFromArray(array);
+		return this;
+	}
+}
+
+if (THREE.Box3.prototype.toArray === undefined) {
+	THREE.Box3.prototype.toArray = function () {
+		return this.min.toArray().concat(this.max.toArray());
 	}
 }
