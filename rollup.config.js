5515f60a670d920a9a4317391d8432377d20271b
Ika <ikatyang@gmail.com>
2016-11-10 20:50:44 +0800

    Support GLSL

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,26 @@
+function glsl() {
+	return {
+		transform (code, id) {
+			if (!/\.glsl$/.test(id)) return;
+			var transformedCode = 'export default ' + JSON.stringify(
+				code
+					.replace(/[ \t]*\/\/.*\n/g, '')
+					.replace(/[ \t]*\/\*[\s\S]*?\*\//g, '')
+					.replace(/\n{2,}/g, '\n')
+			) + ';';
+			return {
+				code: transformedCode,
+				map: { mappings: '' }
+			};
+		}
+	};
+}
+
+
 export default {
 	entry: 'src/Badminton.js',
 	dest: 'build/three.badminton.js',
 	moduleName: 'THREE.Badminton',
-	format: 'umd'
+	format: 'umd',
+	plugins: [ glsl() ]
 };
