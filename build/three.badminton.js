ac2a01c91a82d8d0a1271ed9e6dd75aa63a903ab
Ika <ikatyang@gmail.com>
2016-12-06 14:05:39 +0800

    Update builds

--- build/three.badminton.js
+++ build/three.badminton.js
@@ -2347,6 +2347,81 @@ Record.prototype = {
 	
 };
 
+function AudioChannel(audio) {
+	
+	this.audio = audio;
+	
+	var context = new AudioContext();
+	
+	var media = context.createMediaElementSource(audio);
+	
+	var leftGain = context.createGain();
+	var rightGain = context.createGain();
+	
+	var splitter = context.createChannelSplitter(2);
+	var merger = context.createChannelMerger(2);
+
+	media.connect(splitter);
+	
+	splitter.connect(leftGain, 0);
+	splitter.connect(rightGain, 1);
+	
+	leftGain.connect(merger, 0, 0);
+	rightGain.connect(merger, 0, 1);
+	
+	merger.connect(context.destination);
+	
+	this.leftGain = leftGain;
+	this.rightGain = rightGain;
+	
+	this._volume = 1;
+	this._leftVolume = 1;
+	this._rightVolume = 1;
+	
+	this.updateVolume();
+}
+
+AudioChannel.prototype = {
+
+	constructor: AudioChannel,
+	
+	get volume() {
+		return this._volume;
+	},
+	set volume(value) {
+		this._volume = value;
+		this.updateVolume();
+	},
+	
+	get leftVolume() {
+		return this._leftVolume;
+	},
+	set leftVolume(value) {
+		this._leftVolume = value;
+		this.updateVolume();
+	},
+	
+	get rightVolume() {
+		return this._rightVolume;
+	},
+	set rightVolume(value) {
+		this._rightVolume = value;
+		this.updateVolume();
+	},
+	
+	updateVolume: function () {
+		this.leftGain.gain.value = this.leftVolume * this.volume;
+		this.rightGain.gain.value = this.rightVolume * this.volume;
+	},
+	
+	setEqualizer: function (value) {
+		this._leftVolume = (value < 0.5) ? 1 : (1 - value) / 0.5;
+		this._rightVolume = (value > 0.5) ? 1 : (value - 0) / 0.5;
+		this.updateVolume();
+	},
+	
+};
+
 exports.ShuttlecockGeometry = ShuttlecockGeometry;
 exports.HyperbolaGeometry = HyperbolaGeometry;
 exports.NetGeometry = NetGeometry;
@@ -2364,6 +2439,7 @@ exports.TargetPoint = TargetPoint;
 exports.NetGroup = NetGroup;
 exports.Game = Game;
 exports.Record = Record;
+exports.AudioChannel = AudioChannel;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
