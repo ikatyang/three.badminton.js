a5555783d3cc4f068e4dd672590749f7c6ebc9d2
ikatyang <ikatyang@gmail.com>
2016-12-10 16:22:12 +0800

    Remove TargetPoint.js

--- src/Badminton.js
+++ src/Badminton.js
@@ -16,7 +16,6 @@ export { Robot } from './objects/Robot.js';
 export { Court } from './objects/Court.js';
 export { Scoreboard } from './objects/Scoreboard.js';
 export { ScoreboardCard } from './objects/ScoreboardCard.js';
-export { TargetPoint } from './objects/TargetPoint.js';
 export { NetGroup } from './objects/NetGroup.js';
 
 export { Game } from './utils/Game.js';
