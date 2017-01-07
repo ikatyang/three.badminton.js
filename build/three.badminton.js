d06b50168d9f750def7deb7c5a9d051f3964d5d8
ikatyang <ikatyang@gmail.com>
2017-01-08 02:18:46 +0800

    Update builds

--- build/three.badminton.js
+++ build/three.badminton.js
@@ -760,9 +760,9 @@ function Shuttlecock(shuttlecockGeometry, material, corkMass, skirtMass) {
 	
 	if (!this.mesh.geometry.boundingSphere)
 		this.mesh.geometry.computeBoundingSphere();
-	this.groundAttenuation = 0.8;
 	this.groundElapsed = 0;
 	this.groundElapsedDelta = 0.5;
+	this.groundRestitutionCoefficient = 0.05;
 	
 	this.init();
 }
@@ -811,9 +811,13 @@ Shuttlecock.prototype = Object.defineProperties(Object.assign(Object.create(THRE
 		return this.flipFrame.localToTarget(new THREE.Vector3(0, 1, 0), this.parent, 'direction').normalize();
 	},
 	
-	impact: function (velocity, normal, attenuation, isGround) {
-	
-		this.velocity.reflect(normal).multiplyScalar(1 - attenuation).add(velocity);
+	impact: function (velocity, normal, restitutionCoefficient, isGround) {
+		
+		var vertical = this.velocity.clone().projectOnVector(normal);
+		var horizontal = this.velocity.clone().projectOnPlane(normal);
+		
+		this.velocity.copy(horizontal).addScaledVector(vertical, -restitutionCoefficient).add(velocity);
+		
 		var yAxis = this.getYAxis();
 		
 		this.updateActive(0);
@@ -890,7 +894,7 @@ Shuttlecock.prototype = Object.defineProperties(Object.assign(Object.create(THRE
 				
 				if (this.groundElapsed > this.groundElapsedDelta) {
 					this.groundElapsed = 0;
-					this.impact(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0), this.groundAttenuation, true);
+					this.impact(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0), this.groundRestitutionCoefficient, true);
 				}
 				
 				if (centerY + this.velocity.y * this.groundElapsedDelta < radius)
@@ -1123,7 +1127,7 @@ function Robot(bodyMesh, racketMesh) {
 		new THREE.Vector3(0, 0, 0));
 	
 	this.shuttlecock = null;
-	this.racketAttenuation = 0.9;
+	this.racketRestitutionCoefficient = 0.1;
 	
 	this.bodySpeed = 1000;
 	this.bodyAngularSpeed = Math.PI * 2;
@@ -1311,7 +1315,7 @@ Robot.prototype = Object.assign(Object.create(THREE.Object3D.prototype), {
 			var normal = link.racket.localToTarget(new THREE.Vector3(0, 0, 1), this.parent, 'direction');
 			var strength = impactSpeed * this.getRacketImpactLength();
 			
-			this.shuttlecock.impact(normal.clone().multiplyScalar(strength), normal, this.racketAttenuation);
+			this.shuttlecock.impact(normal.clone().multiplyScalar(strength), normal, this.racketRestitutionCoefficient);
 			
 			this.impactCount = this.shuttlecock.impactCount;
 			
